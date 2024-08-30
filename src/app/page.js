d57eac65d813e6823392034c2f3af2f8d95bc4f8
Eloquence98 <ibneenayat1998@gmail.com>
2024-08-30 20:09:09 +0500

    fix: correct font variable usage on the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
-import { satoshiBlack } from "@/fonts/font-styles"
 
 function Home() {
   return (
     <>
-      <h1 className={`${satoshiBlack.className} text-primary-shade-10 text-7xl`}>heading</h1>
+      <h1 className="font-satoshi font-black text-primary-shade-10 text-7xl">heading</h1>
       <h1 className="text-primary-shade-20">heading</h1>
       <h1 className="text-primary-shade-30">heading</h1>
       <h1 className="text-primary-shade-40">heading</h1>
