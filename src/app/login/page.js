399e0d8e1edbed8a944a72cf25505530438f298a
Sanjay Kumar <sanjay1402k@gmail.com>
2024-08-30 21:29:02 +0500

    dev

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -7,7 +7,22 @@ export const metadata = {
 function Login() {
     return (
         <>
-            <Heading>Login page</Heading>
+       <div className="h-screen flex gap-2">
+                <div className="flex-[1.4] bg-white h-5/6 rounded-[36px] flex flex-col items-center justify-center">
+                    <Heading className="text-primary text-5xl font-extrabold">
+                        Intera<span className="text-primary-tint-60">ctive</span>
+                    </Heading>
+                    <Heading className="text-primary text-5xl font-extrabold">
+                        Illust<span className="text-primary-tint-60">ration</span>
+                    </Heading>
+                </div>
+                
+                <div className="flex-[1] bg-white h-5/6 rounded-[36px] flex flex-col items-center justify-center">
+                    <Heading className="text-primary bg-primary-to-r from-primary-500 font-extrabold">
+                        Form
+                    </Heading>
+                </div>
+            </div>
         </>
     )
 }
