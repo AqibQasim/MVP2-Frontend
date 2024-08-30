ab5cc79c3c8454c04ec2395fc41ea4f444d06b79
Eloquence98 <ibneenayat1998@gmail.com>
2024-08-30 16:59:28 +0500

    feat: add font lufga and styles

--- src/fonts/font-styles.js
+++ src/fonts/font-styles.js
@@ -2,32 +2,82 @@ import localFont from "next/font/local";
 
 export const satoshiRegular = localFont({
     src: "./satoshi/satoshi-regular.woff2",
-    subsets: ["latin"],
     display: 'swap',
     variable: "--font-satoshi-regular",
 })
 
 export const satoshiBlack = localFont({
     src: "./satoshi/satoshi-black.woff2",
-    subsets: ["latin"],
     display: 'swap',
     variable: "--font-satoshi-black",
 })
 export const satoshiBold = localFont({
     src: "./satoshi/satoshi-bold.woff2",
-    subsets: ["latin"],
     display: 'swap',
     variable: "--font-satoshi-bold",
 })
 export const satoshiLight = localFont({
     src: "./satoshi/satoshi-light.woff2",
-    subsets: ["latin"],
     display: 'swap',
     variable: "--font-satoshi-light",
 })
 export const satoshiMedium = localFont({
     src: "./satoshi/satoshi-medium.woff2",
-    subsets: ["latin"],
     display: 'swap',
     variable: "--font-satoshi-medium",
+})
+
+// LUFGA
+export const lufgaBlack = localFont({
+    src: "./lufga/lufga-black.woff2",
+    display: 'swap',
+    variable: "--font-lufga-black",
+})
+
+export const lufgaBold = localFont({
+    src: "./lufga/lufga-bold.woff2",
+    display: 'swap',
+    variable: "--font-lufga-bold",
+})
+
+export const lufgaExtrabold = localFont({
+    src: "./lufga/lufga-extrabold.woff2",
+    display: 'swap',
+    variable: "--font-lufga-extrabold",
+})
+
+export const lufgaExtralight = localFont({
+    src: "./lufga/lufga-extralight.woff2",
+    display: 'swap',
+    variable: "--font-lufga-extralight",
+})
+
+export const lufgaLight = localFont({
+    src: "./lufga/lufga-light.woff2",
+    display: 'swap',
+    variable: "--font-lufga-light",
+})
+
+export const lufgaMedium = localFont({
+    src: "./lufga/lufga-medium.woff2",
+    display: 'swap',
+    variable: "--font-lufga-medium",
+})
+
+export const lufgaRegular = localFont({
+    src: "./lufga/lufga-regular.woff2",
+    display: 'swap',
+    variable: "--font-lufga-regular",
+})
+
+export const lufgaSemibold = localFont({
+    src: "./lufga/lufga-semibold.woff2",
+    display: 'swap',
+    variable: "--font-lufga-semibold",
+})
+
+export const lufgaThin = localFont({
+    src: "./lufga/lufga-thin.woff2",
+    display: 'swap',
+    variable: "--font-lufga-thin",
 })
