6e657297b6acce5d4c2a1a91949984da8b10b464
Eloquence98 <ibneenayat1998@gmail.com>
2024-09-04 22:32:39 +0500

    fix: restrict scrolling to main element in client dashboard layout - Updated the layout to use full screen height, allowing only the main content area to scroll

--- src/app/client/[clientId]/layout.js
+++ src/app/client/[clientId]/layout.js
@@ -4,7 +4,7 @@ import { PAGE_HEIGHT_FIX } from "@/utils/utility";
 function layout({ children }) {
   return (
     <div
-      className={`${PAGE_HEIGHT_FIX} grid grid-cols-[17.0625rem_1fr] grid-rows-[max-content_1fr] gap-[6px]`}
+      className={`${PAGE_HEIGHT_FIX} grid !h-[calc(100dvh-2.25rem)] grid-cols-[17.0625rem_1fr] grid-rows-[max-content_1fr] gap-[6px] overflow-hidden`}
     >
       <header className="rounded-4xl bg-neutral-white p-4">
         <ClientHeader />
@@ -12,7 +12,10 @@ function layout({ children }) {
       <aside className="col-start-1 row-span-2 row-start-1 rounded-4xl bg-neutral-white p-4">
         ASIDE
       </aside>
-      <div className="bg-transparent"> {children} </div>
+      <div className="body-scroll overflow-y-scroll rounded-3xl bg-transparent">
+        {" "}
+        {children}{" "}
+      </div>
     </div>
   );
 }
