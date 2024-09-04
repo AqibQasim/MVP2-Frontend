e87ab85565797bcee5e8e6f7f9522dd94ffeeb1b
Eloquence98 <ibneenayat1998@gmail.com>
2024-09-04 17:17:18 +0500

    prettier plugin saves

--- src/components/ClientPage.js
+++ src/components/ClientPage.js
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import { getDummyClientById, getDummyClients } from "@/lib/tempData";
 import ClientRecommendationCard from "@/components/ClientRecommendationCard";
-import ClientJobsTable from "./ClientJobsTable";
 import Overlay from "@/components/Overlay";
+import { getDummyClientById } from "@/lib/tempData";
 import { useEffect, useState } from "react";
+import ClientJobsTable from "./ClientJobsTable";
 import InterViewScheduler from "./InterviewScheduler";
 import SuccessModal from "./SuccessModal";
 
@@ -15,7 +15,7 @@ const ClientPage = async ({ params }) => {
 
   const handleSuccessAcknowledge = () => {
     setSuccessAcknowledge(!successAcknowledge);
-  }
+  };
 
   const handleOpenOverlay = () => {
     setOverlayVisible(true);
@@ -25,7 +25,6 @@ const ClientPage = async ({ params }) => {
     setOverlayVisible(false);
     setSuccessAcknowledge(false);
   };
-
 
   useEffect(() => {
     const fetchClient = async () => {
@@ -37,48 +36,64 @@ const ClientPage = async ({ params }) => {
   }, [params.clientId]);
 
   useEffect(() => {
-    console.log('[isOverlayVisible]:', isOverlayVisible);
+    console.log("[isOverlayVisible]:", isOverlayVisible);
   }, [isOverlayVisible]);
 
   if (!client) return <div>Loading...</div>;
 
-
-
   // ALL THE CONTENT FOR THE PROPS.z
 
-  const mainHeading = <span>Your call & job request successfully <span style={{
-    backgroundImage: 'linear-gradient(to right, #4624E0, white)',
-    WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent',
-    display: 'inline'
+  const mainHeading = (
+    <span>
+      Your call & job request successfully{" "}
+      <span
+        style={{
+          backgroundImage: "linear-gradient(to right, #4624E0, white)",
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+          display: "inline",
+        }}
+      >
+        sent.
+      </span>
+    </span>
+  );
 
-  }}>sent.</span></span>;
-
-  let text = <>Our representor will soon contact you. You’ll receive an email on <span className="font-semibold">janedoe@gmail.com</span></>;
+  let text = (
+    <>
+      Our representor will soon contact you. You’ll receive an email on{" "}
+      <span className="font-semibold">janedoe@gmail.com</span>
+    </>
+  );
 
   return (
     <>
       <div className="space-y-2">
-        <ClientRecommendationCard handleOpenOverlay={handleOpenOverlay} client={client} />
+        <ClientRecommendationCard
+          handleOpenOverlay={handleOpenOverlay}
+          client={client}
+        />
         <ClientJobsTable />
       </div>
-      <Overlay isVisible={isOverlayVisible} >
-        {
-          successAcknowledge ? (
-            <>
-              <SuccessModal
-                mainHeading={mainHeading}
-                text={text}
-                onClose={handleCloseOverlay} />
-            </>
-          ) : (
-            <>
-              <InterViewScheduler onSuccessAck={handleSuccessAcknowledge} onClose={handleCloseOverlay} />
-            </>
-          )
-        }
+      <Overlay isVisible={isOverlayVisible}>
+        {successAcknowledge ? (
+          <>
+            <SuccessModal
+              mainHeading={mainHeading}
+              text={text}
+              onClose={handleCloseOverlay}
+            />
+          </>
+        ) : (
+          <>
+            <InterViewScheduler
+              onSuccessAck={handleSuccessAcknowledge}
+              onClose={handleCloseOverlay}
+            />
+          </>
+        )}
       </Overlay>
     </>
   );
-}
+};
 export default ClientPage;
