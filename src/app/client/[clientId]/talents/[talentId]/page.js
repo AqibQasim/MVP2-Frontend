86a03b91d89296c2a7485be6e1192b82a26e5809
Eloquence98 <ibneenayat1998@gmail.com>
2024-09-09 17:57:59 +0500

    feat: add client talents details page add heading, mini profile and description

--- src/app/client/[clientId]/talents/[talentId]/page.js
+++ src/app/client/[clientId]/talents/[talentId]/page.js
@@ -1,5 +1,40 @@
+import ButtonBack from "@/components/ButtonBack";
+import Capsule from "@/components/Capsule";
+import EntityCard from "@/components/EntityCard";
+import Heading from "@/components/Heading";
+import Hr from "@/components/Hr";
+import IconWithBg from "@/components/IconWithBg";
+import TalentDescription from "@/components/TalentDescription";
+
 function TalentIdPage() {
-  return <div></div>;
+  return (
+    <div className="min-h-full space-y-4 rounded-3xl bg-neutral-white p-6">
+      <div className="top flex items-center justify-start gap-3">
+        <ButtonBack />
+        <Heading sm>Profile Overview</Heading>
+        <Capsule className="ml-auto !bg-grey-primary-tint-90 !text-primary-tint-10">
+          Trial - 1st April - 14 April
+        </Capsule>
+      </div>
+      <Hr />
+      <div className="mini-profile flex items-center justify-start">
+        <EntityCard
+          entity={{
+            image: "/avatars/avatar-1.png",
+            name: "John Doe",
+            profession: "Software Developer",
+          }}
+        />
+        <Capsule className="ml-auto mt-auto" icon={<IconWithBg icon="$" />}>
+          $20hr
+        </Capsule>
+      </div>
+      <TalentDescription
+        description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae velit numquam hic consectetur amet non sequi repellat? Porro, autem illo, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae velit numquam hic consectetur amet non sequi repellat? Porro, autem illo, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae velit numquam hic consectetur amet non sequi repellat? Porro, autem illo,Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae velit numquam hic consectetur amet non sequi repellat? Porro, autem illo,"
+        skills={["javascript", "react", "python"]}
+      />
+    </div>
+  );
 }
 
 export default TalentIdPage;
--- src/components/TalentDescription.js
+++ src/components/TalentDescription.js
@@ -1,19 +1,22 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 import Heading from "./Heading";
 
-function TalentDescription({ description, isShowMoreEnabled }) {
+function TalentDescription({ description }) {
+  const [showmore, setShowmore] = useState(false);
   return (
-    <div className="justify-between rounded-xl bg-grey-primary-tint-90 px-4 py-4.5">
-      <Heading style={{ color: "#8992A3" }} className="py-3 font-lufga" sm>
-        Description
-      </Heading>
-
-      <div className="text-sm text-grey-primary-shade-30">
-        {description.length >= 300 && !isShowMoreEnabled
-          ? description.slice(0, 300) + "..."
-          : description}
+    <>
+      <div className="justify-between rounded-xl bg-grey-primary-tint-90 px-4 py-4.5">
+        <Heading className="py-3 font-lufga !text-[#8992A3]" sm>
+          Description
+        </Heading>
+        <p className="text-sm text-grey-primary-shade-30">
+          {description.length >= 300 && !showmore
+            ? description.slice(0, 300) + "..."
+            : description}
+        </p>
       </div>
-    </div>
+    </>
   );
 }
 
