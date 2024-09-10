95385128e20953aad09a88f920c8f88d5c230a2c
Sanjay Kumar <sanjay1402k@gmail.com>
2024-09-10 04:26:07 -0700

    dev phase

--- src/components/AvailabilityDropdown.js
+++ src/components/AvailabilityDropdown.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ButtonDown from '@/svgs/ButtonDown';
 
 function AvailabilityDropdown({
   options = [], // List of options for the dropdown
@@ -7,26 +8,33 @@ function AvailabilityDropdown({
   ...rest
 }) {
   const commonClasses =
-    "border border-gray-300 bg-[#F2F1F8] text-gray-900 rounded-full focus:ring-primary focus:border-primary block py-3 px-10 transition-colors duration-300 ease-in-out";
+    "border border-gray-300 bg-[#F2F1F8] text-gray-900 rounded-full focus:ring-primary focus:border-primary block py-3 px-10 transition-colors duration-300 ease-in-out custom-select";
 
   return (
-    <>
-    <select
-      className={`${commonClasses} ${className}`}
-      {...rest}
-    >
-      {placeholder && (
-        <option value="" disabled>
-          {placeholder}
-        </option>
-      )}
-      {options.map((option, index) => (
-        <option key={index} value={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </select>
-    </>
+    <div className={`relative ${className}`}>
+      <div className="icon-container">
+        <div className="bg-white rounded-full h-7 w-7"></div>
+      </div>
+      <select
+        className={commonClasses}
+        {...rest}
+      >
+        {placeholder && (
+          <option value="">
+            {placeholder}
+          </option>
+        )}
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    
+      <div className="custom-arrow ">
+      <ButtonDown />
+</div>
+    </div>
   );
 }
 
