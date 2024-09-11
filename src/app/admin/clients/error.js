c8b64f68e1ccd36fbdc21eeb3712584c4fcba2e8
Eloquence98 <ibneenayat1998@gmail.com>
2024-09-11 23:09:30 +0500

    fix: add error block for admin/clients page

---
+++ src/app/admin/clients/error.js
@@ -0,0 +1,15 @@
+"use client";
+
+export default function Error({ error, reset }) {
+  return (
+    <main className="flex flex-col items-center justify-center gap-6">
+      <h1 className="text-3xl font-semibold">Something went wrong!</h1>
+      <p>while getting Clients</p>
+      <p className="text-lg">{error.message}</p>
+
+      <button className="inline-block px-6 py-3 text-lg" onClick={reset}>
+        Try again
+      </button>
+    </main>
+  );
+}
