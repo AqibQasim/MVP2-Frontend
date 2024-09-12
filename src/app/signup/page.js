04568e9003a72215f2b33c9066debde60042363e
aahilalwani25 <alwani.aahil25@gmail.com>
2024-09-12 15:48:30 +0500

    navigation work

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -43,14 +43,15 @@ function SignUp() {
       if (Object.values(errors).some((err) => err !== "")) {
         return; // Do not proceed with signup if there are validation errors
       }
-      console.log(payload);
+      //console.log(payload);
       const result = await mvp2ApiHelper(payload);
+      console.log(result)
       if (result.status === 200) {
         console.log("signed up successfully");
         setOverlayVisible(true);
       }
     },
-    [payload, errors],
+    [payload, errors,user_role],
   );
 
   const handleOpenOverlay = () => {
