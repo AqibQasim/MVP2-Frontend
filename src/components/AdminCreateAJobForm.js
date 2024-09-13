96f0e60d9eb0891bc61a90ecf2483490cc56ee07
Eloquence98 <ibneenayat1998@gmail.com>
2024-09-14 01:46:06 +0500

    fix: add missing fields and improve validations in job creation

--- src/components/AdminCreateAJobForm.js
+++ src/components/AdminCreateAJobForm.js
@@ -5,6 +5,7 @@ import SubmitButton from "./SubmitButton";
 
 function AdminCreateAJobForm({ clientId }) {
   const [skills, setSkills] = useState([""]);
+  const [applicationQuestions, setApplicationQuestions] = useState([""]);
 
   const handleSkillChange = (index, event) => {
     const newSkills = [...skills];
@@ -12,35 +13,49 @@ function AdminCreateAJobForm({ clientId }) {
     setSkills(newSkills);
   };
 
+  const handleQuestionChange = (index, event) => {
+    const newQuestions = [...applicationQuestions];
+    newQuestions[index] = event.target.value;
+    setApplicationQuestions(newQuestions);
+  };
+
   const addSkillInput = () => {
     setSkills([...skills, ""]);
+  };
+
+  const addQuestionInput = () => {
+    setApplicationQuestions([...applicationQuestions, ""]);
   };
 
   const removeSkillInput = (index) => {
     const newSkills = skills.filter((_, i) => i !== index);
     setSkills(newSkills);
   };
+
+  const removeQuestionInput = (index) => {
+    const newQuestions = applicationQuestions.filter((_, i) => i !== index);
+    setApplicationQuestions(newQuestions);
+  };
+
   return (
     <>
       <h2 className="mb-4 text-xl font-bold">Create Job</h2>
       <form action={createAJobAction}>
         <div className="mb-4">
-          <label htmlFor="clientId" className="hidden">
-            <input
-              type="hidden"
-              id="clientId"
-              name="clientId"
-              value={clientId}
-            />
-          </label>
+          <input
+            type="hidden"
+            id="clientId"
+            name="client_id"
+            value={clientId}
+          />
         </div>
         <div className="mb-4">
-          <label htmlFor="jobTitle" className="block text-gray-700">
+          <label htmlFor="position" className="block text-gray-700">
             Job Title
           </label>
           <input
-            id="jobTitle"
-            name="jobTitle"
+            id="position"
+            name="position"
             type="text"
             className="w-full rounded-full border p-2"
             required
@@ -86,48 +101,48 @@ function AdminCreateAJobForm({ clientId }) {
             className="w-full rounded-full border p-2"
             required
           >
-            <option value="full time">Full Time</option>
-            <option value="part time">Part Time</option>
+            <option value="full-time">Full Time</option>
+            <option value="part-time">Part Time</option>
           </select>
         </div>
 
         <div className="mb-4">
-          <label htmlFor="estLength" className="block text-gray-700">
-            Est Length (weeks)
+          <label htmlFor="project_length" className="block text-gray-700">
+            Project Length (in months)
           </label>
           <input
-            id="estLength"
-            name="estLength"
+            id="project_length"
+            name="project_length"
             className="w-full rounded-full border p-2"
             type="number"
-            min="1"
+            placeholder="e.g. 1"
             required
           />
         </div>
 
         <div className="mb-4">
-          <label htmlFor="jobType" className="block text-gray-700">
+          <label htmlFor="job_type" className="block text-gray-700">
             Job Type
           </label>
           <select
-            id="jobType"
-            name="jobType"
+            id="job_type"
+            name="job_type"
             className="w-full rounded-full border p-2"
             required
           >
             <option value="remote">Remote</option>
-            <option value="on site">On Site</option>
+            <option value="on-site">On Site</option>
             <option value="hybrid">Hybrid</option>
           </select>
         </div>
 
         <div className="mb-4">
-          <label htmlFor="startDate" className="block text-gray-700">
+          <label htmlFor="start_date" className="block text-gray-700">
             Desired Start Date
           </label>
           <input
-            id="startDate"
-            name="startDate"
+            id="start_date"
+            name="start_date"
             className="w-full rounded-full border p-2"
             type="date"
             required
@@ -135,18 +150,45 @@ function AdminCreateAJobForm({ clientId }) {
         </div>
 
         <div className="mb-4">
-          <label htmlFor="workdayOverlap" className="block text-gray-700">
-            Workday Overlap (Hours)
+          <label htmlFor="location" className="block text-gray-700">
+            Location
           </label>
           <input
-            id="workdayOverlap"
-            name="workdayOverlap"
+            id="location"
+            name="location"
             className="w-full rounded-full border p-2"
-            type="number"
-            placeholder="5 hours required"
-            min="0"
+            type="text"
             required
           />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="workday_overlap" className="block text-gray-700">
+            Workday Overlap (in Hours)
+          </label>
+          <input
+            id="workday_overlap"
+            name="workday_overlap"
+            className="w-full rounded-full border p-2"
+            type="number"
+            placeholder="e.g. 5"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="is_test_required" className="block text-gray-700">
+            Is Test Required
+          </label>
+          <select
+            id="is_test_required"
+            name="is_test_required"
+            className="w-full rounded-full border p-2"
+            required
+          >
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
         </div>
 
         <div className="mb-4">
@@ -165,7 +207,6 @@ function AdminCreateAJobForm({ clientId }) {
                 placeholder={`Skill ${index + 1}`}
                 required
               />
-
               {skills.length > 1 && (
                 <button
                   type="button"
@@ -186,8 +227,47 @@ function AdminCreateAJobForm({ clientId }) {
           </button>
         </div>
 
+        <div className="mb-4">
+          <label
+            htmlFor="application_questions"
+            className="block text-gray-700"
+          >
+            Application Questions
+          </label>
+          {applicationQuestions.map((question, index) => (
+            <div key={index} className="mb-2 flex items-center">
+              <input
+                id={`question${index}`}
+                name="application_questions"
+                type="text"
+                className="w-full rounded-full border p-2"
+                value={question}
+                onChange={(event) => handleQuestionChange(index, event)}
+                placeholder={`Question ${index + 1}`}
+                required
+              />
+              {applicationQuestions.length > 1 && (
+                <button
+                  type="button"
+                  className="ml-2 rounded-full bg-red-500 p-2 text-white"
+                  onClick={() => removeQuestionInput(index)}
+                >
+                  Remove
+                </button>
+              )}
+            </div>
+          ))}
+          <button
+            className="rounded-full bg-green-600 p-1 text-white"
+            onClick={addQuestionInput}
+            type="button"
+          >
+            Add more
+          </button>
+        </div>
+
         <div className="flex justify-end">
-          <SubmitButton pendingLabel="Updating...">Update profile</SubmitButton>
+          <SubmitButton pendingLabel="Creating...">Create Jon</SubmitButton>
         </div>
       </form>
     </>
--- src/lib/actions.js
+++ src/lib/actions.js
@@ -2,25 +2,27 @@
 
 // Server Actions
 export async function createAJobAction(formData) {
-  // For testing
-  await new Promise((res) => setTimeout(res, 5000));
-  const clientId = formData.get("clientId");
-  const jobTitle = formData.get("jobTitle");
+  const client_id = formData.get("client_id");
+  const position = formData.get("position");
   const description = formData.get("description");
   const experience = formData.get("experience");
   const commitment = formData.get("commitment");
-  const estLength = formData.get("estLength");
-  const jobType = formData.get("jobType");
-  const desiredStartDate = formData.get("startDate");
-  const workdayOverlap = formData.get("workdayOverlap");
+  const project_length = Number(formData.get("project_length")); // Convert to number
+  const job_type = formData.get("job_type");
+  const start_date = formData.get("start_date");
+  const workday_overlap = Number(formData.get("workday_overlap")); // Convert to number
   const skills = formData.getAll("skills");
-  console.log("Client ID is correct?: ", clientId);
+  const location = formData.get("location");
+  const is_test_required = formData.get("is_test_required") === "true";
+  const applied_customers_count = 0; // default to 0
+  const application_questions = formData.getAll("application_questions");
 
-  if (!clientId || clientId.trim() === "") {
+  // Validate required fields
+  if (!client_id || client_id.trim() === "") {
     throw new Error("Client Id is required.");
   }
 
-  if (!jobTitle || jobTitle.trim() === "") {
+  if (!position || position.trim() === "") {
     throw new Error("Job title is required.");
   }
 
@@ -35,43 +37,60 @@ export async function createAJobAction(formData) {
     throw new Error("Valid experience level is required.");
   }
 
-  if (!commitment || !["full time", "part time"].includes(commitment)) {
+  if (!commitment || !["full-time", "part-time"].includes(commitment.trim())) {
     throw new Error("Valid commitment is required.");
   }
 
-  if (!estLength || isNaN(estLength) || estLength <= 0) {
-    throw new Error("Estimated length must be a valid number.");
+  if (!project_length || isNaN(project_length) || project_length <= 0) {
+    throw new Error("Project length is required and must be a valid number.");
   }
 
-  if (!jobType || !["remote", "on site", "hybrid"].includes(jobType)) {
+  if (!job_type || !["remote", "on-site", "hybrid"].includes(job_type)) {
     throw new Error("Valid job type is required.");
   }
 
-  if (!desiredStartDate || isNaN(new Date(desiredStartDate))) {
+  if (!start_date || isNaN(new Date(start_date))) {
     throw new Error("A valid desired start date is required.");
   }
 
-  if (!workdayOverlap || isNaN(workdayOverlap) || workdayOverlap <= 0) {
-    throw new Error("Workday overlap must be a valid number.");
+  if (!workday_overlap || isNaN(workday_overlap) || workday_overlap <= 0) {
+    throw new Error("Workday overlap is required and must be a valid number.");
+  }
+
+  if (!location || location.trim() === "") {
+    throw new Error("Location is required.");
   }
 
   if (skills.length === 0 || skills.some((skill) => skill.trim() === "")) {
     throw new Error("At least one valid skill is required.");
   }
 
-  console.log("Job Data", {
-    clientId,
-    jobTitle,
+  if (
+    application_questions.length === 0 ||
+    application_questions.some((q) => q.trim() === "")
+  ) {
+    throw new Error("At least one valid application question is required.");
+  }
+
+  // Prepare job data
+  const createJobData = {
+    client_id,
+    position,
     description,
     experience,
     commitment,
-    estLength,
-    jobType,
-    desiredStartDate,
-    workdayOverlap,
+    project_length: `${project_length} Month`,
+    job_type,
+    start_date,
+    workday_overlap: `${workday_overlap} Hours`,
     skills,
-  });
+    location,
+    is_test_required,
+    applied_customers_count,
+    application_questions,
+    status: "active",
+  };
 
-  //   api call here
-  //
+  // Add logic to dispatch the job data or handle it accordingly
+  console.log("Job data prepared for dispatch:", createJobData);
 }
