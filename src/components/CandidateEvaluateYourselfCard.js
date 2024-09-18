c40db3c2b986f3e62c69538d2990a91d2479ed85
aahilalwani25 <alwani.aahil25@gmail.com>
2024-09-18 17:59:31 +0500

    evaluate yourself screen completed

--- src/components/CandidateEvaluateYourselfCard.js
+++ src/components/CandidateEvaluateYourselfCard.js
@@ -5,16 +5,15 @@ import ButtonCapsule from "./ButtonCapsule";
 
 function CandidateEvaluateYourselfCard() {
   return (
-    <div className="flex h-full w-full gap-8 rounded-4xl bg-neutral-white px-8 py-10">
-      <div className="h-full w-full">
-        <Heading sm className="font-[500]" style={{ fontSize: "40px" }}>
-          Here’s Where Your Journey Begins
-        </Heading>
+    <div className="h-full w-full gap-8 rounded-4xl bg-neutral-white px-8 py-10">
+      <Heading sm className="font-[500]" style={{ fontSize: "40px" }}>
+        Here’s Where Your Journey Begins
+      </Heading>
 
-        <div className="h-[324px] w-[815px] gap-8">
-          <div
-            className={`relative size-[140px] overflow-hidden rounded-full bg-bg-avatar`}
-          >
+      <div className="flex h-full w-full flex-col items-center justify-center">
+        <div className="flex h-auto w-auto flex-col items-center justify-center space-y-6">
+          {/* Profile Image */}
+          <div className="relative h-[140px] w-[140px] overflow-hidden rounded-full bg-bg-avatar">
             <Image
               src={"/avatars/avatar-4.svg"}
               fill
@@ -24,21 +23,29 @@ function CandidateEvaluateYourselfCard() {
             />
           </div>
 
-          
-            <Heading sm className="font-[500]" style={{ fontSize: "34px" }}>
+          {/* Welcome Message */}
+          <div className="text-center">
+            <Heading sm className="font-medium" style={{ fontSize: "34px" }}>
               Welcome, Richard Feynman
             </Heading>
             <p className="text-grey-primary-shade-30">
               Are you ready to tackle the AI Assessment to stand out amongst
               other candidates?
             </p>
-          
-          <button
-            className={`flex flex-row items-center justify-between gap-2 rounded-[2.5rem] bg-primary px-5 py-2 text-sm font-bold capitalize text-neutral-white`}
-          >
-            Evaluate yourself
-            <Image width={23} height={24.3} src={"/evaluate-yourself.svg"} />
-          </button>
+          </div>
+
+          {/* Evaluate Yourself Button */}
+          <div>
+            <button className="flex flex-row items-center justify-between gap-2 rounded-full bg-primary px-5 py-2 text-sm font-bold capitalize text-neutral-white">
+              Evaluate yourself
+              <Image
+                width={23}
+                height={24.3}
+                src={"/evaluate-yourself.svg"}
+                alt="Evaluate yourself icon"
+              />
+            </button>
+          </div>
         </div>
       </div>
     </div>
