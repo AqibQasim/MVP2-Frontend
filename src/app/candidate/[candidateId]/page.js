469697bc50c322f4d595e8f7fc3d5591fc449b4e
Eloquence98 <ibneenayat1998@gmail.com>
2024-09-23 17:39:28 +0500

    feat: add Candidate Profile popup - Implement a popup for candidate profile details - Create a new ElementComponent - Modify the existing Modal component styles

--- src/app/candidate/[candidateId]/page.js
+++ src/app/candidate/[candidateId]/page.js
@@ -1,26 +1,10 @@
 "use client";
 import CandidateEvaluateYourselfCard from "@/components/CandidateEvaluateYourselfCard";
-import CandidatePage from "@/components/CandidatePage";
-import { getDummyClientById, getDummyClients } from "@/lib/tempData";
 
-// export async function generateMetadata({ params }) {
-//   const {
-//     company: { name },
-//   } = await getDummyClientById(params.candidateId);
-//   return { title: `Company ${name}` };
-// }
-
-// export async function generateStaticParams() {
-//   const candidates = await getDummyClients();
-//   const ids = candidates.map((candidate) => ({ cabinId: String(candidate.id) }));
-//   return ids;
-// }
-
-export default async function Page({ params }) {
+export default function Page({ params }) {
   return (
     <>
-    <CandidateEvaluateYourselfCard/>
-      {/* <CandidatePage params={params} /> */}
+      <CandidateEvaluateYourselfCard />
     </>
   );
 }
--- src/components/ButtonBack.js
+++ src/components/ButtonBack.js
@@ -2,18 +2,19 @@
 import SvgIconChevronLeft from "@/svgs/SvgIconChevronLeft";
 import { useRouter } from "next/navigation";
 
-function ButtonBack({children, className}) {
+function ButtonBack({ children, className, onClick }) {
   const router = useRouter();
   function handleBack() {
+    if (onClick) return onClick?.();
     router.back();
   }
   return (
     <button
       onClick={handleBack}
-      className={`${className} rounded-full flex-row bg-primary-tint-100 p-3 text-neutral-dark`}
+      className={`${className} flex items-center justify-center gap-2 rounded-full bg-primary-tint-100 p-3 text-neutral-dark`}
     >
       {" "}
-      <SvgIconChevronLeft className="size-3.5" />{children}{" "}
+      <SvgIconChevronLeft className="size-3.5" /> <span>{children}</span>
     </button>
   );
 }
