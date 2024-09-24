058d4936e5b165e95d9b56b366c8ce8126e9027d
Eloquence98 <ibneenayat1998@gmail.com>
2024-09-24 14:25:48 +0500

    chore: log error when fetching all recommended candidates

--- src/app/client/[clientId]/recommended/page.js
+++ src/app/client/[clientId]/recommended/page.js
@@ -4,8 +4,10 @@ import { getAllRecommendedCandidates } from "@/lib/data-service";
 async function Page({ params }) {
   const clientId = params.clientId;
   console.log("params: ", clientId);
-  const recommendedCandidatesForJobs =
+  const { data: recommendedCandidatesForJobs, error } =
     await getAllRecommendedCandidates(clientId);
+
+  if (error) console.log("Error: getting recommeneded Candidates: ", error);
 
   console.log("on page: ", recommendedCandidatesForJobs);
   return (
