0922075c2f4f8df795cb5e4c1d7d2179dc09936d
Eloquence98 <ibneenayat1998@gmail.com>
2024-10-02 21:49:46 +0500

    feat: add AuthProvider to manage Google session for user authentication - ensure dynamic rendering prevention for authenticated user access

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import { AuthProvider } from "@/components/AuthContext";
 import { lufga, satoshi } from "@/fonts/font-styles";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
 export const metadata = {
   title: {
@@ -11,14 +13,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="max-h-[100dvh] overflow-hidden bg-heavy-metal">
-      <body
-        className={`${satoshi.variable} ${lufga.variable} body-scroll mx-auto h-[100dvh] max-w-[1920px] overflow-x-hidden overflow-y-scroll rounded-[48px] bg-neutral-white p-3 !pr-[calc(0.75rem-12px)] font-satoshi font-normal`}
-      >
-        <main className="min-h-full rounded-[36px] bg-bg-frame p-[6px]">
-          {children}
-        </main>
-      </body>
-    </html>
+    <SessionProvider>
+      <AuthProvider>
+        <html
+          lang="en"
+          className="max-h-[100dvh] overflow-hidden bg-heavy-metal"
+        >
+          <body
+            className={`${satoshi.variable} ${lufga.variable} body-scroll mx-auto h-[100dvh] max-w-[1920px] overflow-x-hidden overflow-y-scroll rounded-[48px] bg-neutral-white p-3 !pr-[calc(0.75rem-12px)] font-satoshi font-normal`}
+          >
+            <main className="min-h-full rounded-[36px] bg-bg-frame p-[6px]">
+              {children}
+            </main>
+          </body>
+        </html>
+      </AuthProvider>
+    </SessionProvider>
   );
 }
--- src/components/LoggedInUserTest.js
+++ src/components/LoggedInUserTest.js
@@ -1,9 +1,14 @@
-import { auth } from "@/lib/auth";
+"use client";
+// import { auth } from "@/lib/auth";
+import { useAuth } from "./AuthContext";
 import Heading from "./Heading";
 
 async function LoggedInUserTest() {
   // this makes route dynamic as it reads cookies that can only be known at run time
-  const session = await auth();
+  // const session = await auth();
+
+  // not this part
+  const session = useAuth();
   if (!session?.user) return null;
 
   const {
