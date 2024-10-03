066813d5e1bc555580367ba5485e274d93043e74
Eloquence98 <ibneenayat1998@gmail.com>
2024-10-03 16:09:31 +0500

    fix: resolve client layout styles - fix logout button styles - render ClientLougout or GoogleLogout based on login method

--- src/components/ClientLogout.js
+++ src/components/ClientLogout.js
@@ -1,8 +1,8 @@
 "use client"; // This makes it a client component
-import { useRouter } from "next/navigation";
 import SvgIconLogout from "@/svgs/SvgIconLogout";
+import { useRouter } from "next/navigation";
 
-export default function ClientLogout() {
+export default function ClientLogout({ className }) {
   const router = useRouter();
 
   const handleLogout = () => {
@@ -15,11 +15,12 @@ export default function ClientLogout() {
   };
 
   return (
-    <div className="p-4" onClick={handleLogout}>
-      <div className="group flex cursor-pointer items-center justify-start gap-3 rounded-[0.625rem] bg-transparent px-4 py-3 font-lufga text-sm font-medium text-grey-primary-shade-20 transition-colors duration-200 hover:bg-primary-tint-100 hover:text-primary-tint-20">
-        <SvgIconLogout className="size-6" />
-        <span>Logout</span>
-      </div>
+    <div
+      className={`${className} group flex cursor-pointer items-center justify-start gap-3 rounded-[0.625rem] bg-transparent px-4 py-3 font-lufga text-sm font-medium text-grey-primary-shade-20 transition-colors duration-200 hover:bg-primary-tint-100 hover:text-primary-tint-20`}
+      onClick={handleLogout}
+    >
+      <SvgIconLogout className="size-6" />
+      <span>Logout</span>
     </div>
   );
 }
