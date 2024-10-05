6dba47fa940465b4523cc58c954fd136fee8f665
Ans Tariq <anstariq333@gmail.com>
2024-10-05 18:56:41 +0500

    Set up update info button and developed bake button

--- src/components/ButtonCapsule.js
+++ src/components/ButtonCapsule.js
@@ -5,13 +5,13 @@ function ButtonCapsule({ className, children, onPress, id, disable=false }) {
     <button
       onClick={onPress}
       disabled={disable}
-      className={`${className} gap-4.5 inline-flex items-center justify-center rounded-[2.5rem] bg-primary p-1.5 pl-5 text-sm font-bold capitalize text-neutral-white`}
+      className={`${className} gap-4.5 inline-flex items-center justify-between rounded-[2.5rem] bg-primary p-1.5 pl-5 text-sm font-bold capitalize text-neutral-white`}
       id={id}
       
     >
       {" "}
       {children}{" "}
-      <span className="rounded-[150px] bg-neutral-white px-4 py-3 text-neutral-dark">
+      <span className="rounded-[150px] bg-neutral-white  px-4 py-3 text-neutral-dark">
         <ChevronRight className="h-[10px] w-[9px]" />
       </span>{" "}
     </button>
