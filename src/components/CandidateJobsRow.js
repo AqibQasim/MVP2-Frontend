fe8615dd445502e02d700598a8fdeea3e031b088
aahilalwani25 <alwani.aahil25@gmail.com>
2024-10-08 20:47:21 +0500

    candidate jobs show

--- src/components/CandidateJobsRow.js
+++ src/components/CandidateJobsRow.js
@@ -14,21 +14,21 @@ function CandidateJobsRow({ job }) {
         icon={<SvgIconWork className="relative -right-[1.3px]" />}
         entity={
           {
-            name: job?.position,
-            profession: job?.specialization,
+            name: job?.job?.position,
+            profession: job?.job?.specialization,
           }
         }
       />
       <div className="skills flex items-center justify-center gap-1.5 text-center">
-        {job.skills.map((skill, i) => (
+        {job?.job?.skills.map((skill, i) => (
           <Skill key={i} skill={skill} />
         ))}
       </div>
-      <div className="experience text-center">{job.experience}</div>
-      <div className="commitment text-center">{job.commitment}</div>
+      <div className="experience text-center">{job?.job?.experience}</div>
+      <div className="commitment text-center">{job?.job?.commitment}</div>
       <Capsule
         className="mr-auto w-max !bg-primary-tint-100"
-        icon={<IconWithBg icon={<SvgIconJobStatus status={job.status} />} />}
+        icon={<IconWithBg icon={<SvgIconJobStatus status={job?.job?.job_status} />} />}
       >
         {" "}
         {job.status}{" "}
--- src/components/CandidateJobsTable.js
+++ src/components/CandidateJobsTable.js
@@ -3,21 +3,25 @@ import { useEffect, useState } from "react";
 import CandidateJobsRow from "./CandidateJobsRow";
 import DashboardSection from "./DashboardSection";
 import Table from "./Table";
+import { useParams } from "next/navigation";
 
 function CandidateJobsTable() {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const params = useParams();
+  const cid = params.candidateId;
 
   useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_REMOTE_URL}/get-job-candidates?job_status=all`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_REMOTE_URL}/get-job-candidates?job_status=hired-and-trial&candidate_id=${cid}`);
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
         const data = await response.json();
-        setJobs(data.data); // assuming the data is in the 'data' field
+        console.log(data)
+        setJobs(data?.data); // assuming the data is in the 'data' field
       } catch (err) {
         setError(err.message);
       } finally {
