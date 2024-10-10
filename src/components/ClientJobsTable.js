e0ed0afd2307b3d428f2bb58d92b32ec6c19df90
Ans Tariq <anstariq333@gmail.com>
2024-10-10 19:14:59 +0500

    add message not data to show at the momment when data not omes from backend

--- src/components/ClientJobsTable.js
+++ src/components/ClientJobsTable.js
@@ -22,10 +22,18 @@ async function ClientJobsTable({client_id}) {
           <div className="status text-center">Status</div>
           <div className="action text-center">Action</div>
         </Table.Header>
+
+        {clientJobs && clientJobs.length > 0 ? (
         <Table.Body
           data={clientJobs.data.result}
           render={(job, i) => <ClientJobsRow job={job} key={i} />}
         />
+         ) : (
+        <div >
+          <p>No data to show at the moment</p>
+        </div>
+        )}
+
       </Table>
     </DashboardSection>
   );
