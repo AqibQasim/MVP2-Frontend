6afa212936ea750a568a8ce1da3a77bf9824860b
AqibQasim <muhammad44aqib@gmail.com>
2024-10-11 16:48:11 +0500

    dev

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -1,19 +1,22 @@
-import AdminCandidatesClientsHiringTable from '@/components/AdminCandidatesClientsHiringTable';
-import AdminCandidatesTable from '@/components/AdminCandidatesTable';
-import { fetchCandidatesJobStatus, fetchRecommendedCandidates } from '@/lib/data-service';
-import React from 'react'
+import AdminCandidatesClientsHiringTable from "@/components/AdminCandidatesClientsHiringTable";
+import { fetchCandidatesJobStatus } from "@/lib/data-service";
+import React from "react";
 
 async function Page() {
-    const { data: candidateJobStatus, error } = await fetchCandidatesJobStatus('hired-and-trial');
-    if (error) {
-        return (
-            <div>
-                {error}
-            </div>
-        )
-    };
-  
-    return <AdminCandidatesClientsHiringTable candidateJobStatus={candidateJobStatus} />;
+  let candidateJobStatus = [];
+  try {
+    const { data, error } = await fetchCandidatesJobStatus("hired-and-trial");
+    if (error) throw new Error(error);
+    candidateJobStatus = data;
+  } catch (err) {
+    return <div>Failed to load candidate job status: {err.message}</div>;
+  }
+
+  return (
+    <AdminCandidatesClientsHiringTable
+      candidateJobStatus={candidateJobStatus}
+    />
+  );
 }
 
-export default Page
+export default Page;
