39392039e90d7ee84d02c32418d682457dbb3805
aahilalwani25 <alwani.aahil25@gmail.com>
2024-11-18 19:23:47 +0500

    dev

--- src/lib/authConfig.js
+++ src/lib/authConfig.js
@@ -31,7 +31,7 @@ export const authConfig = {
       const loginPage = pathname === "/login";
       const signupPage = pathname === "/signup";
       const googleUserRedirectPath = user
-        ? `/${user.user_role=== "client" ? credentialUser.user_role : "candidate"}/${user[`${user.user_role}_id`]}`
+        ? `/${user.user_role=== "client" ? "client/"+user[`client_id`] : "candidate/"+user[`customer_id`]}`
         : null;
 
       const credentialUserRedirectPath = credentialUser?.id
