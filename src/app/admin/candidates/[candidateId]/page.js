3dd5170b07b521d931c2fded914bfee5cb0d0a6f
Aahil Alwani <alwani.aahil25@gmail.com>
2024-12-05 13:49:54 +0500

    edit price work

--- src/app/admin/candidates/[candidateId]/page.js
+++ src/app/admin/candidates/[candidateId]/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
 //import ButtonBack from "./ButtonBack";
 import Capsule from "@/components/Capsule";
@@ -23,6 +23,8 @@ import { fetchClientJobs, getClients } from "@/lib/data-service";
 import { mvp2ApiHelper } from "@/Helpers/mvp2ApiHelper";
 import AdminCandidateJobHistory from "@/components/AdminCandidateJobHistory";
 import getCandidateStatus from "@/utils/getCandidateStatus";
+import ButtonCapsule from "@/components/ButtonCapsule";
+import ErrorPopup from "@/components/ErrorPopup";
 
 function Page({ params }) {
   const [talent, setTalent] = useState(null);
@@ -40,6 +42,11 @@ function Page({ params }) {
   const [isClientsShow, setIsClientShow] = useState(false);
   const [isJobsShow, setIsJobsShow] = useState(false);
   const [jobHistory, setJobHistory] = useState(null);
+  const [isEditPrice, setIsEditPrice] = useState(false);
+  const [editedPrice, setEditedPrice] = useState(talent?.hourly_rate);
+  const router= useRouter();
+  const [alert,setAlert]= useState(null)
+  //const [error,setError]= useState(null)
 
   const filteredClients = clients?.filter((client) =>
     client.name.toLowerCase().includes(searchClient.toLowerCase()),
@@ -82,6 +89,35 @@ function Page({ params }) {
     console.log(response?.data?.data);
     setJobHistory(response?.data?.data);
     //console.log(jobHistory)
+  }, []);
+
+  const saveEditedPrice = useCallback(async (price) => {
+    if(isNaN(price)||price===null){
+      setError("price can only be a number")
+      setAlert(true);
+    }else{
+      const payload = {
+        endpoint: `profile-info-update/${customer_id}`,
+        method: "PUT",
+        body: {
+          hourly_rate: price,
+        },
+      };
+  
+      try {
+        const result = await mvp2ApiHelper(payload);
+  
+        if (result.status === 200) {
+          console.log("Price updated successfully!");
+        } else {
+          console.error("Failed to update profile.");
+        }
+      } catch (error) {
+        console.error("Error while updating profile:", error);
+      }
+      setIsEditPrice(false);
+      router?.refresh();
+    }
   }, []);
 
   useEffect(() => {
@@ -221,8 +257,14 @@ function Page({ params }) {
 
           <Capsule className="ml-auto !bg-grey-primary-tint-90 !text-primary-tint-10">
             {getCandidateStatus(talent?.talent_status, talent?.status)}
-            {talent?.talent_status !== "open" &&
-              talent?.talent_status !== "interviewing" && (
+            {getCandidateStatus(
+              talent?.talent_status,
+              talent?.status,
+            ).toLowerCase() !== "available" &&
+              getCandidateStatus(
+                talent?.talent_status,
+                talent?.status,
+              ).toLowerCase() !== "interviewing" && (
                 <>
                   {" "}
                   {formatDate(talent?.updatedAt)} - {newEndTrialDate}
@@ -231,7 +273,7 @@ function Page({ params }) {
           </Capsule>
         </div>
         <Hr />
-        <div className="mini-profile flex items-center justify-start">
+        <div className="mini-profile flex items-center justify-between">
           <EntityCard
             entity={{
               image: "/avatars/avatar-1.png",
@@ -239,9 +281,29 @@ function Page({ params }) {
               profession: talent?.specialization,
             }}
           />
-          <Capsule className="ml-auto mt-auto" icon={<IconWithBg icon="$" />}>
-            ${talent?.hourly_rate}hr
-          </Capsule>
+          {!isEditPrice ? (
+            <Capsule className="ml-auto mt-auto" icon={<IconWithBg icon="$" />}>
+              ${talent?.hourly_rate}hr
+              <div
+                onClick={() => setIsEditPrice(true)}
+                className="cursor-pointer"
+              >
+                <Image src={"/icons/icon-edit.svg"} width={20} height={20} />
+              </div>
+            </Capsule>
+          ) : (
+            <div className="gap-2">
+              <input
+                //type="number"
+                className="rounded-[2.25rem] border-2 border-black px-4 py-3 text-sm font-medium capitalize"
+                value={editedPrice}
+                onChange={(event) => setEditedPrice(event.target.value)}
+              />
+              <ButtonCapsule onPress={() => saveEditedPrice(editedPrice)}>
+                Save
+              </ButtonCapsule>
+            </div>
+          )}
         </div>
 
         <div className="flex flex-row justify-center">
@@ -433,6 +495,13 @@ function Page({ params }) {
           </div>
         </form>
       </Modal>
+      {alert && (
+        <ErrorPopup
+          message={error}
+          type="error"
+          onClose={() => setAlert(false)}
+        />
+      )}
     </>
   );
 }
