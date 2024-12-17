8c604f17734b7ab2b0b9ea83d441666295fb4d35
Eloquence98 <ibneenayat1998@gmail.com>
2024-12-17 16:17:04 +0500

    fix: resolve input highlight color on error on login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -25,9 +25,6 @@ function Login() {
   const [isForgotPasswordOpened, setIsForgotPasswordOpened] = useState(false);
   const [show, setShow] = useState(false);
 
-  
-
-
   // useEffect(() => {
   //   // console.log("Requesting notification permission...");
   //   requestNotificationPermission();
@@ -208,7 +205,6 @@ function Login() {
       }
     },
     [form, errors, user_role],
-
   );
 
   return (
@@ -260,6 +256,7 @@ function Login() {
               type="text"
               name="email"
               value={form.email}
+              error={errors.email}
               onChange={handleChange}
               placeholder="Enter your email"
               className="mt-5"
@@ -274,6 +271,7 @@ function Login() {
                 value={form.password}
                 onKeyDown={handleKeyDown} // Trigger login on Enter
                 onChange={handleChange}
+                error={errors.password}
                 placeholder="Enter your password"
                 className="mt-3"
               />
